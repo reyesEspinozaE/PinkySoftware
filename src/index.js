ac4811057dc82970446dcb0e3c3b2eebbf5aed71
Everth Reyes E. <128534063+reyesEspinozaE@users.noreply.github.com>
2024-06-09 04:50:04 -0600

    updt
    Start working on uploads images

--- src/index.js
+++ src/index.js
@@ -1,68 +1,53 @@
-// import express from 'express';
-// import { dirname, join } from 'path'; // Con join podemos concatenar sin importar el SO
-// import { fileURLToPath } from 'url';
-// // Se importa el enrutador desde /routes/index.js
-// import indexRoutes from './routes/index.js';
-// // Importa tu archivo de asociaciones
-// import './models/associations.js';
-
-// const app = express();
-// const __dirname = dirname(fileURLToPath(import.meta.url)); // Con esto obtengo la ruta del proyecto
-
-// // Configuración del motor de plantillas y vistas
-// app.set('views', join(__dirname, 'views'));
-// app.set('view engine', 'ejs'); // motor de plantilla ejs, en vez de html
-
-// // Middleware para parsear solicitudes con cuerpos JSON y URL codificados
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Uso de las rutas
-// app.use(indexRoutes);
-
-// console.log(__dirname);
-
-// // Servir archivos estáticos desde la carpeta public
-// // Cualquier solicitud a / será manejada por este middleware, que buscará los archivos en la carpeta 'public'
-// app.use(express.static(join(__dirname, 'public')));
-
-// app.listen(3000, () => {
-//     console.log('Server is listening on port 3000');
-// });
-
-
-
 import express from 'express';
-import session from 'express-session';
-import { dirname, join } from 'path';
+import { dirname, join } from 'path'; // Con join podemos concatenar sin importar el SO
 import { fileURLToPath } from 'url';
+// Se importa el enrutador desde /routes/index.js
 import indexRoutes from './routes/index.js';
+// Importa tu archivo de asociaciones
 import './models/associations.js';
+import multer from 'multer';
+import fs from 'node:fs';
+
+// {dest: 'uploads/'}, es para obtener las rutas de los ficheros (IMG)
+const upload = multer({dest: 'uploads/'});
 
 const app = express();
+
+// Recibir las peticiones de IMG
+app.post('/images/single', upload.single('imagenPerfil'), (req, res) => {
+    console.log(req.file);
+    saveImage(req.file);
+    res.send('Termina img');
+});
+
+// Funcion para recibir los datos de las img
+function saveImage(file) {
+    const newPath = `./uploads/${file.originalname}`;
+    fs.renameSync(file.path, newPath);
+    return newPath;
+}
+
+// Con esto obtengo la ruta del proyecto, en mi local  (PC)
+// Es decir la ruta de almacenamiento
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Configuración del motor de plantillas y vistas
 app.set('views', join(__dirname, 'views'));
+
+// Motor de plantilla ejs, en vez de html
 app.set('view engine', 'ejs');
 
+// Middleware para parsear solicitudes con cuerpos JSON y URL codificados
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware de sesión
-app.use(session({
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: false
-}));
-
-// Middleware para añadir el usuario a las vistas
-app.use((req, res, next) => {
-    res.locals.usuario = req.session.user;
-    next();
-});
-
+// Uso de las rutas
 app.use(indexRoutes);
 
+console.log(__dirname);
+
+// Servir archivos estáticos desde la carpeta public
+// Cualquier solicitud a / será manejada por este middleware, que buscará los archivos en la carpeta 'public'
 app.use(express.static(join(__dirname, 'public')));
 
 app.listen(3000, () => {
